544ee0a6325d320bc30c4768acc83c37cb9e0a41
Usama Ali <usama.route@gmail.com>
2023-07-13 21:38:49 +0300

    adding index.html, index.js

---
+++ main.js
@@ -0,0 +1,6 @@
+//                0         1         2        3
+var friends = ["ahmed", "mohamed", "omar", "khaled", "hazem", "Hamada"]; // ==> 4
+
+for (var i = 0; i < friends.length; i++) {
+  console.log(friends[i]);
+}
